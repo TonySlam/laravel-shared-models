fa2c9f53d319a55babce84769a535289778ca994
tonymoakamela <tony@plusnarrative.com>
2023-02-23 17:51:15 +0200

    My package

--- src/LinkModels.php
+++ src/LinkModels.php
@@ -6,7 +6,7 @@ use PDO;
 
 class LinkModels
 {
-    private $host = 'localhost';
+    /*private $host = 'localhost';
     private $port = '3306';
 
 
@@ -18,6 +18,26 @@ class LinkModels
     }
 
     public  function getModels()
+    {
+        $pdo = $this->connect();
+        $stmt = $pdo->query('SELECT * FROM models');
+        $models = $stmt->fetchAll(PDO::FETCH_CLASS, 'App\\Model\\Model');
+        return $models;
+    }*/
+    private $host = 'localhost';
+    private $port = '3306';
+    private $database = 'stores-db';
+    private $username = 'root';
+    private $password = '';
+
+    public function connect()
+    {
+        $dsn = "mysql:host={$this->host};port={$this->port};dbname={$this->database}";
+        $pdo = new PDO($dsn, $this->username, $this->password);
+        return $pdo;
+    }
+
+    public function getModels()
     {
         $pdo = $this->connect();
         $stmt = $pdo->query('SELECT * FROM models');
