d17aad00966f865877fd4c7c3f2ea9ac09acd0c9
tonymoakamela <tony@plusnarrative.com>
2023-02-23 18:34:39 +0200

    My package

--- src/LinkModels.php
+++ src/LinkModels.php
@@ -6,7 +6,27 @@ use PDO;
 
 class LinkModels
 {
-    public static function connect($host = '127.0.0.1', $port = '3306', $database, $username, $password = null)
+    /*private $host = 'localhost';
+    private $port = '3306';
+
+
+    public static function connect($host, $port, $database, $username, $password = null)
+    {
+        $dsn = "mysql:host={$host};port={$port};dbname={$database}";
+        $pdo = new PDO($dsn, $username, $password);
+        return $pdo;
+    }
+
+    public  function getModels()
+    {
+        $pdo = $this->connect();
+        $stmt = $pdo->query('SELECT * FROM models');
+        $models = $stmt->fetchAll(PDO::FETCH_CLASS, 'App\\Model\\Model');
+        return $models;
+    }*/
+
+
+    private function connect($host , $port , $database, $username, $password = null)
     {
         $dsn = "mysql:host={$host};port={$port};dbname={$database}";
         $pdo = new PDO($dsn, $username, $password);
