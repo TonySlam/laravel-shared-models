5f8e715b8801add6f7afc5d920269d6b9b0f4fd8
tonymoakamela <tony@plusnarrative.com>
2023-02-23 18:22:07 +0200

    My package

--- src/LinkModels.php
+++ src/LinkModels.php
@@ -6,34 +6,10 @@ use PDO;
 
 class LinkModels
 {
-    /*private $host = 'localhost';
-    private $port = '3306';
-
-
-    public static function connect($host, $port, $database, $username, $password = null)
+    public function connect($host = '127.0.0.1', $port = '3306', $database, $username, $password = null)
     {
         $dsn = "mysql:host={$host};port={$port};dbname={$database}";
         $pdo = new PDO($dsn, $username, $password);
-        return $pdo;
-    }
-
-    public  function getModels()
-    {
-        $pdo = $this->connect();
-        $stmt = $pdo->query('SELECT * FROM models');
-        $models = $stmt->fetchAll(PDO::FETCH_CLASS, 'App\\Model\\Model');
-        return $models;
-    }*/
-    private $host = 'localhost';
-    private $port = '3306';
-    private $database = 'laravel-db';
-    private $username = 'root';
-    private $password = '';
-
-    public function connect()
-    {
-        $dsn = "mysql:host={$this->host};port={$this->port};dbname={$this->database}";
-        $pdo = new PDO($dsn, $this->username, $this->password);
         return $pdo;
     }
 
