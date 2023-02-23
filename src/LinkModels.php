71de66101de62aa426208bc22c00ee6b98f480a8
tonymoakamela <tony@plusnarrative.com>
2023-02-23 18:23:48 +0200

    My package

--- src/LinkModels.php
+++ src/LinkModels.php
@@ -6,7 +6,7 @@ use PDO;
 
 class LinkModels
 {
-    public function connect($host = '127.0.0.1', $port = '3306', $database, $username, $password = null)
+    public static function connect($host = '127.0.0.1', $port = '3306', $database, $username, $password = null)
     {
         $dsn = "mysql:host={$host};port={$port};dbname={$database}";
         $pdo = new PDO($dsn, $username, $password);
